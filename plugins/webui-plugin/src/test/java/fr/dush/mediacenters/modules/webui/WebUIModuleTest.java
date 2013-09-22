e7d3511dc142180e7d4ab05e232dc0833df9d05b
Thomas Duchatelle <duchatelle.thomas@gmail.com>
2013-09-22 15:07:21 +0200

    [WEBUI] Implement LifeCycle Service to provide CDI Container before it is initilized by daemon.

--- plugins/webui-plugin/src/test/java/fr/dush/mediacenters/modules/webui/WebUIModuleTest.java
+++ plugins/webui-plugin/src/test/java/fr/dush/mediacenters/modules/webui/WebUIModuleTest.java
@@ -1,12 +1,30 @@
 package fr.dush.mediacenters.modules.webui;
 
+import static org.fest.assertions.api.Assertions.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ConnectException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.CDI;
+
 import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
+import fr.dush.mediacenters.modules.webui.controllers.ProviderData;
 import fr.dush.mediamanager.tools.CDIUtils;
 
 public class WebUIModuleTest {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(WebUIModuleTest.class);
+
+	private final String USER_AGENT = "Mozilla/5.0";
 
 	static {
 		SLF4JBridgeHandler.removeHandlersForRootLogger();
@@ -14,21 +32,51 @@ public class WebUIModuleTest {
 	}
 
 	@Test
-	@Ignore("Use mvn jetty:run to test application.")
-	public void testStartingServer() throws Exception {
+	@Ignore("Long test ...")
+	public void testNewInstanciate() throws Exception {
+		WebUIModule launcher = new WebUIModule();
+		launcher.beforeStartCdi(null);
 
-		System.setProperty("mediamanager.propertiesfile", "../../mediamanager-core/src/test/resources/dbconfig-int.properties");
+		assertThat(sendGet("http://localhost:8080/")).isEqualTo(200);
+
+		final CDI<Object> weld = CDI.current();
+		final BeanManager beanManager = weld.getBeanManager();
+		LOGGER.info("Bean manager found : {}", beanManager);
 
 		CDIUtils.bootCdiContainer();
+		final ProviderData providerData = CDIUtils.getBean(beanManager, ProviderData.class);
+		LOGGER.info("Provider data : {}", providerData);
+
+		CDIUtils.stopCdiContainer();
+		launcher.afterStopCdi();
+
+		assertThat(sendGet("http://localhost:8080/")).isEqualTo(-1);
+	}
+
+	private int sendGet(String url) throws Exception {
+
+		HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		// Read content...
 		try {
-			WebUIModule launcher = CDIUtils.getBean(WebUIModule.class);
-			launcher.startServer();
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
 
-			launcher.join();
-			// Thread.sleep(30000);
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine.trim());
+			}
+			in.close();
 
-		} finally {
-			CDIUtils.stopCdiContainer();
+			// print result
+			LOGGER.info("Response : {}", response);
+
+			return con.getResponseCode();
+
+		} catch (ConnectException e) {
+			return -1;
 		}
 	}
 }
